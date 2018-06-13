c7f3aaeaa0eddd475e2b0f7d761720306fb2a21c
Kersti McGee <kerstimcgee@gmail.com>
2018-06-13 12:06:33 -0600

    changes added, caesar code incl.

--- main.py
+++ main.py
@@ -1,9 +1,55 @@
-from flask import Flask
+from flask import Flask, request, redirect
+import cgi
+
 app = Flask(__name__)
+
 app.config['DEBUG'] = True
+
+page_header = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Rotate it!</title>
+        <style> 
+        form {
+            background-color: #eee;
+            padding: 20px;
+            margin: 0 auto;
+            width: 540px;
+            font: 16px sans-serif;
+            border-radius: 10px;
+            }
+        textarea {
+            margin: 10px 0;
+            width: 540px;
+            height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+"""
+add_form = """
+    <form action="/rotate" method="post">
+        <label>
+            Rotate by:
+            <input type="number" name="rot" value="0"/>
+        </label>
+        <br>
+            <textarea name="some_text_to_rot" form="user_form">Enter text here</textarea> 
+        <br>
+            <input type="submit" value="Submit Query"/>  
+    </body>
+</html>
+"""
 
 @app.route("/")
 def index():
-    return "Hello World"
-
+    content = add_form + page_header
+    return content
+    
 app.run()
+
+#@app.route("/rotated")
+
+
+
